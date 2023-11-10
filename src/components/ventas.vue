d4e5115efcd7e9159eacc6e89c38f2dd912fa14a
EdwinDG <halg830@gmail.com>
2023-11-10 17:43:52 -0500

    EDG

--- src/components/ventas.vue
+++ src/components/ventas.vue
@@ -1,3 +1,51 @@
 <template>
     <button>awaeff</button>
 </template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRutaStore } from "../stores/ruta.js";
+import { useBusStore } from '../stores/bus.js';
+
+
+const busStore = useBusStore()
+const rutaStore = useRutaStore()
+
+
+async function obtenerBus() {
+  try {
+    const response = await busStore.obtener();
+    if (Array.isArray(response.buses)) {
+      const empresasAsignadas = response.buses.map(bus => `${bus.empresa_asignada} / ${bus.n_asiento}`);
+      opcionesBuses.value = empresasAsignadas;
+      rows.value = empresasAsignadas;
+    } else {
+      console.error('Los datos de los buses no son un array válido:', response.buses);
+    }
+  } catch (error) {
+    console.error('Error al obtener los buses:', error);
+  }
+}
+
+
+async function obtenerRuta() {
+  try {
+    const responseRuta = await rutaStore.obtener();
+    if (Array.isArray(responseRuta)) {
+      const origenRutaDestino = responseRuta.map(ruta => `${ruta.origen} - ${ruta.destino} / ${ruta.horarios}`);
+      opcionesRutas.value = origenRutaDestino;
+      rows.value = origenRutaDestino;
+    } else {
+      console.error('Los datos de los ruta no son un array válido:', responseRuta);
+    }
+  } catch (error) {
+    console.error('Error al obtener los ruta:', error);
+  }
+}
+
+onMounted(() => {
+  obtenerBoleto();
+  obtenerRuta();
+  obtenerBus(); 
+});
+</script>
